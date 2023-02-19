f68dda14dcf99b16f15da670ce04905ffd9f17dc
Ikaro Felix <ikaroisdm@gmail.com>
2023-02-19 20:44:13 -0300

    Day 2
    Add footer and more option to the page and make the more option open when clicked using JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+const body = document.body
+const html = document.documentElement
+const more = document.querySelector(".more")
+const addOptions = document.getElementById("add-options")
+
+more.addEventListener("click", function(){
+    if (addOptions.classList.contains("options-active")) {
+        addOptions.classList.remove("options-active");
+        more.classList.remove("options-active")
+    } else {
+        more.classList.add("options-active")
+        addOptions.classList.add("options-active");
+    }
+    
+})
